7bc1afefa91b653406ce2056de5a6f68133c4cc0
Nikolay Ivanov <32921584+NIKSANAIVANOV@users.noreply.github.com>
2017-12-24 12:56:24 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
 
-namespace Packman_Game
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+
+
+namespace Program
+
 {
-    static class Program
+
+    class Program
+
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+
+        public static void Main(String[] args)
+
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            int p, lastInt = 0, input;
+
+            Console.WriteLine("Enter the Number of Rows : ");
+
+            input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= input; i++)
+
+            {
+
+                for (p = 1; p <= i; p++)
+
+                {
+
+                    if (lastInt == 1)
+
+                    {
+
+                        Console.Write("0");
+
+                        lastInt = 0;
+
+                    }
+
+                    else if (lastInt == 0)
+
+                    {
+
+                        Console.Write("1");
+
+                        lastInt = 1;
+
+                    }
+
+                }
+                Console.Write("\n");
+
+            }
+
+            Console.ReadLine();
+
         }
+
     }
+
 }
